b863e085c3c64a3f09e5d4e676f77bd7d16b5616
Marcus Silveira <marcusvinicius.767.mv@gmail.com>
2024-02-29 23:53:21 -0300

    criado Todo para testas o Asp.net

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers;
+
+[Route("api/todo")]
+[ApiController]
+public class TodoController: ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok();
+    }
+}
---
+++ Models/TodoModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Todo.Models;
+
+public class TodoModel
+{
+    public int Id { get; private set; }
+    public string Title { get; private set; }
+    public bool Done { get; private set; }
+    public DateTime CreatedAt { get; set; }
+}
